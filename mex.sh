49b079fced42b726782136af6f771a747f545c8b
remotomasi <remo.tomasi@gmail.com>
2018-02-20 17:50:04 +0100

    Improved management of the files. Create simple html file

--- mex.sh
+++ mex.sh
@@ -30,7 +30,7 @@ read par mer
 echo -e " Date Time Temp DewP Hum Cloud Rain WindS WindDir WindDirDeg Sky Press Low Med High ChanceRain ChanceSnow ZeroLevel" > DATAS/finalDatas.csv
 
 # download of json datas and save the wjson file
-wget "http://ws1.metcheck.com/ENGINE/v9_0/json.asp?lat=$par&lon=$mer&lid=22553" 2>/dev/null -O - > DATAS/wjson.json
+wget "http://ws1.metcheck.com/ENGINE/v9_0/json.asp?lat=$par&lon=$mer" 2>/dev/null -O - > DATAS/wjson.json # &lid=22553
 
 # loop to obtain infos from the json file: here we use jq
 i=0
@@ -43,7 +43,7 @@ done
 # finishing the informations formatting datas in various formats
 cat DATAS/dati.csv | tr '\n' ',' | tr '\t' '\n' | tr ',' '\t' | sed 's/:00:00.00//g' | tr ' ' '-' | tr 'T' '\t' | tr '\t' ' ' >> DATAS/finalDatas.csv
 sed -i '2d' DATAS/finalDatas.csv                                                    # first two lines
-cat DATAS/finalDatas.csv | cut -d' ' -f2- > DATAS/tmp.csv                           # temporary file to eliminate first cloumn
+cat DATAS/finalDatas.csv | cut -d' ' -f2- > DATAS/tmp.csv                           # temporary file to eliminate first column
 cat DATAS/tmp.csv > DATAS/finalDatas.csv                                            # transfer in a new file
 head -74 DATAS/finalDatas.csv > DATAS/weatherForecast.csv                           # 74: only 3 days of forecasting - 116: for 5 days
 cp DATAS/weatherForecast.csv mexDATAS/weatherForecast_$par-$mer-$DATE.csv           # Archive csv in mexDATAS
@@ -51,37 +51,40 @@ awk 'BEGIN{FS=OFS=" "}{print $1,$2,$3,$4,$5,$6,$7,int($8*1.609),$9,$10,$11,$12,$
 sed -i 's/n 0 W/n WindS W/g' DATAS/tmp.csv                                          # column from 0 to WindS
 cat DATAS/tmp.csv > DATAS/weatherForecast.csv
 
-# select the only important infos
+# select the only important infos for a human readable meteo
 cut -d' ' -f1,2,3,5,6,7,8,9,11,16 DATAS/weatherForecast.csv > DATAS/weatherForecastLight.csv
 
-# manage columns
-awk '{ print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10}' DATAS/weatherForecastLight.csv > HTMLS/p.txt
+# managing columns
+#awk '{ print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10}' DATAS/weatherForecastLight.csv > DATAS/wfl_tmp.csv
 # take only infos in particular moment of the day: h00-h06-h12-h18
-cat DATAS/weatherForecastLight.csv | grep '^...........00\|^...........06\|^...........12\|^...........18' > HTMLS/p.txt
+cat DATAS/weatherForecastLight.csv | grep '^...........00\|^...........06\|^...........12\|^...........18' > DATAS/wfl_tmp.csv
 
 # add the columns
-paste -d'\n' HTMLS/columns.txt HTMLS/p.txt > HTMLS/p2.txt
+cat columns.txt > DATAS/wfl_tmp2.csv
 
 # sorting columns
-awk '{ print $1,$2,$3,$4,$5,$6,$10,$7,$8,$9}' HTMLS/p2.txt > HTMLS/p.txt
+awk '{ print $1,$2,$3,$4,$5,$6,$10,$7,$8,$9}' DATAS/wfl_tmp.csv >> DATAS/wfl_tmp2.csv
+cat DATAS/wfl_tmp2.csv > DATAS/weatherForecastLight.csv
 
 # convert in various formats
-./conv2htm.sh HTMLS/p.txt > HTMLS/p.html
+#./conv2htm.sh HTMLS/p.txt > HTMLS/p.html
 ./conv2htm.sh DATAS/weatherForecastLight.csv > HTMLS/weatherForecastLight.html
-sed -i 's/nowrap >/nowrap ><h2>/g' HTMLS/weatherForecastLight.html
-sed -i 's/td>/td><\/h2>/g' HTMLS/weatherForecastLight.html
+#sed -i 's/nowrap >/nowrap ><h3>/g' HTMLS/weatherForecastLight.html
+#sed -i 's/td>/td><\/h2>/g' HTMLS/weatherForecastLight.html
+
+# Temperatures
 
 echo "Data file downloaded and formatted..."
-./conv2htm.sh DATAS/weatherForecast.csv > HTMLS/weatherForecast.html           # csv to html conversion
-xvfb-run --server-args="-screen 0, 1024x768x24" cutycapt --url=file://$PWD/HTMLS/weatherForecast.html --out=IMAGES/weatherForecastData.png
-./graph.pg > IMAGES/weatherForecastGraph.png                                   # creating graph by gnuplot
-./cloud.pg > IMAGES/weatherForecastCloud.png                                   # graph for clouds
-./rain.pg > IMAGES/weatherForecastRain.png                                     # graph for rain
-./pressureWind.pg > IMAGES/weatherForecastPressureWind.png                     # graph pressure/wind
-./pressureZeroLevel.pg > IMAGES/weatherForecastPressureZeroLevel.png           # graph pressure/zerolevel
-./tempDewPoint.pg > IMAGES/weatherForecastTempDewPoint.png                     # graph pressure/zerolevel
-./wind.pg > IMAGES/weatherForecastWind.png                                     # graph wind direction/power
-# final image composition of the 3 previous graphs
-# convert weatherForecastGraph.png weatherForecastCloud.png weatherForecastPressure.png +append weatherForecastFinal.png
-convert \( IMAGES/weatherForecastGraph.png IMAGES/weatherForecastRain.png -append \) \( IMAGES/weatherForecastCloud.png IMAGES/weatherForecastPressureZeroLevel.png -append \) \( IMAGES/weatherForecastWind.png IMAGES/weatherForecastTempDewPoint.png -append \) \( IMAGES/weatherForecastPressureWind.png -append \) +append IMAGES/weatherForecastFinal.png
-rm DATAS/tmp.csv  # remove temporary or useless files
+#./conv2htm.sh DATAS/weatherForecast.csv > HTMLS/weatherForecast.html           # csv to html conversion
+#xvfb-run --server-args="-screen 0, 1024x768x24" cutycapt --url=file://$PWD/HTMLS/weatherForecast.html --out=IMAGES/weatherForecastData.png
+# ./graph.pg > IMAGES/weatherForecastGraph.png                                   # creating graph by gnuplot
+# ./cloud.pg > IMAGES/weatherForecastCloud.png                                   # graph for clouds
+# ./rain.pg > IMAGES/weatherForecastRain.png                                     # graph for rain
+# ./pressureWind.pg > IMAGES/weatherForecastPressureWind.png                     # graph pressure/wind
+# ./pressureZeroLevel.pg > IMAGES/weatherForecastPressureZeroLevel.png           # graph pressure/zerolevel
+# ./tempDewPoint.pg > IMAGES/weatherForecastTempDewPoint.png                     # graph pressure/zerolevel
+# ./wind.pg > IMAGES/weatherForecastWind.png                                     # graph wind direction/power
+# # final image composition of the 3 previous graphs
+# # convert weatherForecastGraph.png weatherForecastCloud.png weatherForecastPressure.png +append weatherForecastFinal.png
+# convert \( IMAGES/weatherForecastGraph.png IMAGES/weatherForecastRain.png -append \) \( IMAGES/weatherForecastCloud.png IMAGES/weatherForecastPressureZeroLevel.png -append \) \( IMAGES/weatherForecastWind.png IMAGES/weatherForecastTempDewPoint.png -append \) \( IMAGES/weatherForecastPressureWind.png -append \) +append IMAGES/weatherForecastFinal.png
+#rm DATAS/tmp.csv DATAS/wfl_tmp.csv DATAS/wfl_tmp2.csv  # remove temporary or useless files
